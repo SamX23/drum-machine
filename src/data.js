31e126d54fdc3e3139d9f7d7861f4a1f6ca8b479
SamX23 <skalexsong@gmail.com>
2022-08-07 22:43:27 +0700

    Update : data sound

--- src/data.js
+++ src/data.js
@@ -5,35 +5,35 @@ const drumData = [
   },
   {
     id: "W",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
   },
   {
     id: "E",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
   },
   {
     id: "A",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
   },
   {
     id: "S",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
   },
   {
     id: "D",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
   },
   {
     id: "Z",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
     id: "X",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
   },
   {
     id: "C",
-    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
   },
 ];
 
